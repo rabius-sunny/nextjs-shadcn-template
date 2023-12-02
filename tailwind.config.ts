7b1f41d92f9c98acb0af6ac9231043672bbe7b0f
rabius-sunny <rabiussunnyc3x@gmail.com>
2023-12-03 01:20:14 +0600

    add: tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,57 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+
+const config: Config = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    colors: {
+      primary: colors.red[500],
+      secondary: colors.indigo[500],
+      success: colors.emerald[400],
+      dark: colors.slate[700],
+      slate: colors.slate,
+      gray: colors.gray,
+      neutral: colors.neutral,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      pink: colors.pink,
+      red: colors.red,
+      black: colors.black,
+      white: colors.white,
+      transparent: colors.transparent,
     },
     extend: {
       keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 }
+
+export default config
